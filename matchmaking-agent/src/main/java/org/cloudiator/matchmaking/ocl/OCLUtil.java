c59ce6e859ad3aff6d8dd550ef0dc6bc3be166c6
Daniel Baur <daniel.baur@uni-ulm.de>
2018-07-26 17:32:11 +0200

    OCLUtil no longer silently swallows exceptions

--- matchmaking-agent/src/main/java/org/cloudiator/matchmaking/ocl/OCLUtil.java
+++ matchmaking-agent/src/main/java/org/cloudiator/matchmaking/ocl/OCLUtil.java
@@ -6,10 +6,13 @@ import org.eclipse.ocl.pivot.utilities.OCL;
 import org.eclipse.ocl.pivot.utilities.ParserException;
 import org.eclipse.ocl.pivot.utilities.Query;
 import org.eclipse.ocl.xtext.essentialocl.EssentialOCLStandaloneSetup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class OCLUtil {
 
   private static OCL ocl = OCL.newInstance(OCL.CLASS_PATH);
+  private static final Logger LOGGER = LoggerFactory.getLogger(OCLUtil.class);
 
   static {
     EssentialOCLStandaloneSetup.doSetup();
@@ -27,8 +30,20 @@ public class OCLUtil {
       throws ParserException {
     try {
       return getOcl().createInvariant(contextElement, expression);
-    } catch (Exception e) {
-      return getOcl().createInvariant(contextElement, expression);
+    } catch (Exception first) {
+      LOGGER.warn(String
+          .format(
+              "Error while creating invariant for contextElement %s and expression %s. Retrying.",
+              contextElement, expression), first);
+      try {
+        return getOcl().createInvariant(contextElement, expression);
+      } catch (Exception second) {
+        LOGGER.error(String
+            .format(
+                "Retry creating of invariant for contextElement %s and expression %s also failed. Giving up with exception.",
+                contextElement, expression), second);
+        throw second;
+      }
     }
   }
 
