6ef23fb63b5739b05a5da66138283b9e41e6685a
Daniel Baur <daniel.baur@uni-ulm.de>
2020-01-02 17:32:14 +0100

    updates

--- matchmaking-agent/src/main/java/org/cloudiator/matchmaking/cmpl/CMPLTemplate.java
+++ matchmaking-agent/src/main/java/org/cloudiator/matchmaking/cmpl/CMPLTemplate.java
@@ -16,6 +16,18 @@ public class CMPLTemplate {
           + "  $2$: private[]T * x[] = privateSize;\n"
           + "  $3$: sum{n in NODES : x[n]} = nodeSize;";
 
+  public static final String SINGLE =
+      "%data : NODES set, nodeSize, costs[NODES]\n"
+          + "\n"
+          + "variables:\n"
+          + " x[NODES]: integer[0..];\n"
+          + "\n"
+          + "objectives:\n"
+          + " cost: costs[]T * x[]->min;\n"
+          + "\n"
+          + "constraints:\n"
+          + "  $1$: sum{n in NODES : x[n]} = nodeSize;\n";
+
   public static final String NODES =
       "%data : NODES set, LOCATIONS set, nodeSize, largerOrEqual4Cores[NODES], costs[NODES], cores[NODES], inLocation[LOCATIONS, NODES]\n"
           + "\n"
